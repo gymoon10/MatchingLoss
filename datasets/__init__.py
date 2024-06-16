70ba6872e976851ca9d9539bdb400d33d470460b
Goo Young Moon <44194558+gymoon10@users.noreply.github.com>
2024-06-16 18:13:05 +0900

    Add files via upload

---
+++ datasets/__init__.py
@@ -0,0 +1,31 @@
+
+from datasets.CornDataset import CornDataset
+from datasets.CornDatasetCrop import CornDatasetCrop
+from datasets.CornDatasetTwoClass import CornDatasetTwoClass
+from datasets.CityscapesDataset import CityscapesDataset
+
+from datasets.CornDatasetCropDistill import CornDatasetCropDistill
+from datasets.CornDatasetCropDual import CornDatasetCropDual
+
+
+def get_dataset(name, dataset_opts):
+    if name == "CornDataset":
+        return CornDataset(**dataset_opts)
+
+    elif name == "CornDatasetCrop":
+        return CornDatasetCrop(**dataset_opts)
+
+    elif name == "CityscapesDataset":
+        return CityscapesDataset(**dataset_opts)
+
+    elif name == "CornDatasetTwoClass":
+        return CornDatasetTwoClass(**dataset_opts)
+
+    elif name == "CornDatasetCropDistill":
+        return CornDatasetCropDistill(**dataset_opts)
+
+    elif name == "CornDatasetCropDual":
+        return CornDatasetCropDual(**dataset_opts)
+
+    else:
+        raise RuntimeError("Dataset {} not available".format(name))
