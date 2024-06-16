efd1fe6b7485e32aa470973f30fd7d9197aa0b61
Goo Young Moon <44194558+gymoon10@users.noreply.github.com>
2024-06-16 18:13:33 +0900

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,11 +1,11 @@
 
 from datasets.CornDataset import CornDataset
-from datasets.CornDatasetCrop import CornDatasetCrop
-from datasets.CornDatasetTwoClass import CornDatasetTwoClass
+# from datasets.CornDatasetCrop import CornDatasetCrop
+# from datasets.CornDatasetTwoClass import CornDatasetTwoClass
 from datasets.CityscapesDataset import CityscapesDataset
 
-from datasets.CornDatasetCropDistill import CornDatasetCropDistill
-from datasets.CornDatasetCropDual import CornDatasetCropDual
+# from datasets.CornDatasetCropDistill import CornDatasetCropDistill
+# from datasets.CornDatasetCropDual import CornDatasetCropDual
 
 
 def get_dataset(name, dataset_opts):
